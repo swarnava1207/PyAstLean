0c6db4d79fad05b454faffb87f61d64b9e291ea3
shubhamkumar13 <kumar.shubham.off@gmail.com>
2026-05-20 16:04:36 +0530

    add test case for PyAstLean

--- PyAstLeanTest/PyAstLeanCheck/Cases/subscript.py
+++ PyAstLeanTest/PyAstLeanCheck/Cases/subscript.py
@@ -6,3 +6,19 @@
 
 arr = [1, 2, 3]
 result = arr[0]
+
+# PYASTLEANCHECK START
+# TARGET: command
+# CHECK: def foo : IO String := do
+# CHECK:   let x := "hi"
+# CHECK:   let y := String.replicate 10 (String.get x 0)
+# CHECK:   let z := String.extract y 2 7
+# CHECK:   return z
+# PYASTLEANCHECK END
+
+def foo():
+    x = "hi"
+    y = x[0]
+    y *= 10
+    z = y[2:-3]
+    return z
