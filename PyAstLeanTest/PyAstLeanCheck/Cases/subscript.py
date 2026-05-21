4680439e647125fef77ca05407a2d20ada92ddd9
shubhamkumar13 <kumar.shubham.off@gmail.com>
2026-05-21 15:33:09 +0530

    add edge case testing for string subscript x[1000:-3000]

--- PyAstLeanTest/PyAstLeanCheck/Cases/subscript.py
+++ PyAstLeanTest/PyAstLeanCheck/Cases/subscript.py
@@ -1,7 +1,9 @@
 # PYASTLEANCHECK START
 # TARGET: command
-# CHECK: def arr := [(1 : Int), (2 : Int), (3 : Int)]
-# CHECK: def result := GetElem.getElem arr (0 : Int)
+# CHECK: def arr : List Int :=
+# CHECK:   [(1 : Int), (2 : Int), (3 : Int)]
+# CHECK: def result : Int :=
+# CHECK:   PyAstLean.pyListGetItem arr (0 : Int)
 # PYASTLEANCHECK END
 
 arr = [1, 2, 3]
@@ -9,11 +11,14 @@ result = arr[0]
 
 # PYASTLEANCHECK START
 # TARGET: command
-# CHECK: def foo : IO String := do
-# CHECK:   let x := "hi"
-# CHECK:   let y := String.replicate 10 (String.get x 0)
-# CHECK:   let z := String.extract y 2 7
-# CHECK:   return z
+# CHECK: def foo : IO String :=
+# CHECK:   Id.run
+# CHECK:     (do
+# CHECK:       let mut x : String := "hi"
+# CHECK:       let mut y : String := PyAstLean.pyListGetItem x (0 : Int)
+# CHECK:       y := y *ₚ (10 : Int)
+# CHECK:       let mut z : String := PyAstLean.pyStringSlice y (some (2 : Int)) (some (-3))
+# CHECK:       return z)
 # PYASTLEANCHECK END
 
 def foo():
@@ -22,3 +27,16 @@ def foo():
     y *= 10
     z = y[2:-3]
     return z
+
+# PYASTLEANCHECK START
+# TARGET: command
+# CHECK: def bar : IO String :=
+# CHECK:   let x : String := "hi"
+# CHECK:   let y : String := PyAstLean.pyStringSlice x (some (100 : Int)) (some (-2000))
+# CHECK:   y
+# PYASTLEANCHECK END
+
+def bar():
+    x = "hi"
+    y = x[100:-2000]
+    return y
